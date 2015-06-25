904e36c9a21dc7e56ae59efd5c005dbd19842b7f
Tommy Dang <quantumventuress@gmail.com>
2015-06-24 17:40:14 -0700

    first commit

---
+++ TicTacToe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TicTacToe
+//
+//  Created by Tommy DANGerous on 6/24/15.
+//  Copyright (c) 2015 Quantum Ventures. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
