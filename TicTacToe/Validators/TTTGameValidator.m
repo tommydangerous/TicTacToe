b56b49f148d89a3eb3b68088899e8d73ea61b3af
Tommy Dang <quantumventuress@gmail.com>
2015-06-25 22:48:18 -0700

    game validator can check vertical

--- TicTacToe/Validators/TTTGameValidator.m
+++ TicTacToe/Validators/TTTGameValidator.m
@@ -19,6 +19,10 @@
   NSNumber *consecutive = @1;
   [self checkHorizontal:playerMove matrix:matrix consecutive:&consecutive];
 
+  if ([consecutive intValue] < GameNeededToWin) {
+    [self checkVertical:playerMove matrix:matrix consecutive:&consecutive];
+  }
+
   if ([consecutive intValue] >= GameNeededToWin) {
     if ([playerMove.value intValue] == 0) {
       return TTTGameValidatorResultPlayer1Victory;
@@ -55,6 +59,31 @@
     rightPointer++;
     NSNumber *valueAtPoint =
       [[matrix objectAtIndex:playerMove.row] objectAtIndex:rightPointer];
+    if ([playerMove.value intValue] == [valueAtPoint intValue]) {
+      *consecutive = @([*consecutive intValue] + 1);
+    }
+  }
+}
+
+- (void)checkVertical:(TTTPlayerMove *)playerMove
+        matrix:(NSArray *)matrix
+        consecutive:(NSNumber **)consecutive
+{
+  int topPointer = playerMove.row;
+  while (topPointer > 0) {
+    topPointer--;
+    NSNumber *valueAtPoint =
+      [[matrix objectAtIndex:topPointer] objectAtIndex:playerMove.column];
+    if ([playerMove.value intValue] == [valueAtPoint intValue]) {
+      *consecutive = @([*consecutive intValue] + 1);
+    }
+  }
+
+  int bottomPointer = playerMove.row;
+  while (bottomPointer < GameRows - 1) {
+    bottomPointer++;
+    NSNumber *valueAtPoint =
+      [[matrix objectAtIndex:bottomPointer] objectAtIndex:playerMove.column];
     if ([playerMove.value intValue] == [valueAtPoint intValue]) {
       *consecutive = @([*consecutive intValue] + 1);
     }
